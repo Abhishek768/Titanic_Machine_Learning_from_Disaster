51113c6bd38bf97cd93bca23129a95927923687f
Abhishek <abhishek82893@gmail.com>
2020-02-17 19:59:47 +0530

    Initial commit

---
+++ titanic.py
@@ -0,0 +1,12 @@
+# import essential libraries 
+import numpy as np
+import pandas as pd
+import matplotlib.pyplot as plt
+import seaborn as snp
+from sklearn.model_selection import train_test_split
+from sklearn.linear_model import LogisticRegression 
+from sklearn.metrics import classification_report, confusion_matrix
+
+# fetch test and train data from csv files
+train_data = pd.read_csv('')
+test_data = pd.read_csv('')
